8e7fa98ec125f0178f08d10ca1a1de6e2da552b4
Sakthi <53593383+sgsakthi1992@users.noreply.github.com>
2025-01-28 15:04:25 +0000

    double or nothing

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,32 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
   public void playGame() {
-    // TODO: Implement method
+    var points = 10;
+    var scanner = new Scanner(System.in);
+    var userInput = "yes";
+    var random = new Random();
+    var isStillPlaying = true;
+    while(isStillPlaying) {
+      System.out.println("Available points: " + points);
+      System.out.println("Do you want to continue (yes/no):");
+      userInput = scanner.nextLine();
+      if(userInput.equalsIgnoreCase("yes")){
+        if(random.nextBoolean()) {
+          points = points * 2;
+        } else {
+          System.out.println("Better luck next time");
+          isStillPlaying = false;
+        }
+      } else{
+        System.out.println("Thanks for playing. Points won: " +points);
+        isStillPlaying = false;
+      }
+    }
+    scanner.close();
   }
 }
