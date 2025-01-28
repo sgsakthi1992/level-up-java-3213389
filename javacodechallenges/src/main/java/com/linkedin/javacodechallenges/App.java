a1826b0fc7bc964eda63cdeea91d428bb69f754c
Sakthi <53593383+sgsakthi1992@users.noreply.github.com>
2025-01-28 16:43:08 +0000

    100 days from now

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,15 @@
 package com.linkedin.javacodechallenges;
 
+import java.time.LocalDate;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
 
     public static void main( String[] args )
-    {
+    {     
         System.out.println("100 days from now is... " 
-            /* add function call */);
+            + LocalDate.now().plusDays(100));
     }
 }
