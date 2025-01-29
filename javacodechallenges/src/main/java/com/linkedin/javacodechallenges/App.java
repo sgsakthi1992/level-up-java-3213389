df285c38663efc1a23d2e51e9bc5611fe73e5bb7
Sakthi <53593383+sgsakthi1992@users.noreply.github.com>
2025-01-29 11:21:00 +0000

    incomplete volunteer events

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -2,13 +2,19 @@ package com.linkedin.javacodechallenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
-        // TODO: implement function
-        return List.of();
+        var attendees = attendeesMapping.values().stream()
+            .flatMap(studtentList -> studtentList.stream())
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return students.stream()
+        .filter(name -> !attendees.containsKey(name) || attendees.get(name) < 2)
+        .toList();
     }
 
     public static void main(String[] args) {
